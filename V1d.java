4101c7bbb9f72ae455dcac433b95a952921b14fa
moraxj <112534126+moraxj@users.noreply.github.com>
2022-09-03 21:37:05 +0530

    Add files via upload

---
+++ V1d.java
@@ -0,0 +1,8 @@
+public class V1d {
+    public void v1d(int n){
+        n = 4;System.out.println("The series is: ");
+       for(int i =1;i<=10;i++){
+        System.out.print(n*i+ "  ");
+       }
+    }
+}
