856b54d298c02f99148587b233a530a7b795aa11
moraxj <112534126+moraxj@users.noreply.github.com>
2022-09-03 21:33:52 +0530

    Add files via upload

---
+++ exprog3.java
@@ -0,0 +1,9 @@
+public class exprog3 {
+    public void expro3(int n){
+       System.err.println("the series is: ");
+        for(int i = 1;i<=10;i++){
+            int m = 3;
+            System.out.print(m*i+" ");
+        }
+    }
+}
